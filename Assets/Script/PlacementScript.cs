8d2be6a3f32979ebdcfaffe3de547531282f95ea
wizerdmanlenny <wizerdmanlenny@gmail.com>
2023-02-20 19:37:42 -0600

    reseting to scratch

---
+++ Assets/Script/PlacementScript.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlacementScript : MonoBehaviour
+{
+    private int selectedObjectInArray;
+    private GameObject currentlySelectedObject;
+    [SerializeField] private GameObject[] selectableObjects;
+
+    private bool isAnObjectSelected = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        selectedObjectInArray = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 spawnPos = new Vector2(Mathf.Round(mousePos.x), Mathf.Round(mousePos.y));
+        if (Input.GetKeyDown("e") && isAnObjectSelected == false)
+        {
+            currentlySelectedObject = (GameObject)Instantiate(selectableObjects[selectedObjectInArray], spawnPos, Quaternion.identity); //instantiating a game object and making a reference to it
+            isAnObjectSelected = true;
+        }
+    }
+}
---
+++ Assets/Script/TemplateScript.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TemplateScript : MonoBehaviour
+{
+    [SerializeField] private GameObject finalObject; //settable object
+
+    private Vector2 mousePos; //get mouse position
+
+    [SerializeField] private LayerMask allTilesLayer; //layer holder
+
+    // Update is called once per frame
+    void Update()
+    {
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition); //get mouse position
+        transform.position = new Vector2(Mathf.Round(mousePos.x), Mathf.Round(mousePos.y)); //to keep grid snap
+
+        if(Input.GetMouseButtonDown(0)) //if left click
+        {
+            Vector2 mouseRay = Camera.main.ScreenToWorldPoint(transform.position);
+            RaycastHit2D rayHit = Physics2D.Raycast(mousePos, Vector2.zero, Mathf.Infinity, allTilesLayer); //what did i hit with my mouse? mousePos seems to work but not mouseRay, odd
+
+            if (rayHit.collider == null) //if i hit nothing
+            {
+                Instantiate(finalObject, transform.position, Quaternion.identity); //create the settable object
+            }
+        }
+    }
+}
